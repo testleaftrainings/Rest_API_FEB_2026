95e8de07d55bdf6dede129f2c0d2f58b0da53a1b
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2026-03-15 16:02:35 +0530

    Added Serialization and Deserialization code into the framework

--- workspace/matschie/src/test/java/com/testleaf/matschie/servicenow/services/Servicenow.java
+++ workspace/matschie/src/test/java/com/testleaf/matschie/servicenow/services/Servicenow.java
@@ -1,5 +1,6 @@
 package com.testleaf.matschie.servicenow.services;
 
+import com.google.gson.Gson;
 import com.testleaf.matschie.rest.assured.api.client.RestAssuredApiClient;
 
 import io.restassured.RestAssured;
@@ -11,13 +12,18 @@ public class Servicenow {
 	
 	private RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
     protected RestAssuredApiClient apiClient = new RestAssuredApiClient();
-	protected Response response;
+    private Gson gson = new Gson();
+    protected Response response;
 	
 	protected RequestSpecification globalConfig() {
 		return requestSpecBuilder.setBaseUri("https://dev324941.service-now.com")
 				                 .setBasePath("/api/now/table/")
 				                 .setAuth(RestAssured.basic("admin", "/eESj0uC3k+O"))
 				                 .build();
+	}
+	
+	protected <T> T deSerializeResponse(String response, Class<T> classType) {
+		return gson.fromJson(response, classType);
 	}
 
 }
